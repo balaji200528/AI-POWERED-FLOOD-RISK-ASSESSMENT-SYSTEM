9bedccacdd24bc3c8fd1244113be2500fb1fbd71
balaji28305 <balaji28305@gmail.com>
2025-08-13 12:30:44 +0530

    header section done

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
+import { Globe } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen bg-gradient-to-br
+     from-slate-50 via-blue-50 to-indigo-100">
+      <div className="container mx-auto px-1 py-8 max-w-6xl">
+
+      {/* Header */}
+      <div className='text-center mb-8'>
+        <div className="flex items-center justify-center mb-4">
+          <div className="p-3 bg-blue-100 rounded-full mr-4">
+              <Globe className="h-8 w-8 text-blue-500" />
+          </div>
+          <h1 className="text-3xl font-bold text-slate-900">Flood detection system</h1>
+        </div>
+        <p className="text-slate-900">Analyze flood risk using coordinate or upload images for AI-powered terrain anaylysis</p>
+      </div>
+      </div>
       <p>hello world</p>
     </div>
   );
